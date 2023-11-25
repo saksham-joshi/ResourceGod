b21d9559d89af3a30ec9e68ed85e29d8992d2e0f
SAKSHAM JOSHI <109752898+saksham-joshi@users.noreply.github.com>
2023-11-25 15:07:13 +0530

    Delete JS directory

--- JS/experi.js
+++
@@ -1,9 +0,0 @@
-var obj = {
-    "joshisaksham27@gmail.com": {
-        "first_name": "Saksham",
-        "last_name": "Joshi",
-        "password": "12345678"
-    }
-};
-
-console.log(JSON.stringify(obj) );
--- JS/request_forum.js
+++
@@ -1,8 +0,0 @@
-function request_received(){
-    var title = document.querySelector("input").value.trim();
-    var desc = document.querySelector("textarea").value.trim();
-    if(title.length <=5 && title>=50){
-        alert("Title must be more than 5 & less than 50 characters");
-        return;
-    }
-}
